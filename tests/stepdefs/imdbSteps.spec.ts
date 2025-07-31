81fd7bdf38312446bfa04c4c57163dc5c46815ca
Alex <nearbyg@gmail.com>
2025-07-31 14:28:26 +0300

    added cases.ts

--- tests/stepdefs/imdbSteps.spec.ts
+++ tests/stepdefs/imdbSteps.spec.ts
@@ -1,35 +1,37 @@
-import { test } from "../fixtures/baseFixture";
-import { expect } from "@playwright/test";
+import { test } from "../fixtures/baseFixture"
+import { expect } from "@playwright/test"
+import cases from "../fixtures/cases"
 
-test.describe("imdb UI steps", () => {
-    test("Search and Validate the movie",
-        async ({homePage, searchPage}) => {
-            const testCase = "bingo"
-            await homePage.searchField.fill(testCase)
-            await homePage.searchField.press("Enter")
-            expect(await (await searchPage.searchKeyword(testCase)).textContent()).toBe(`Search \"${testCase}\"`)
+test.describe("Search and Validate the movie", () => {
+    cases.testCases.forEach((testCase) => {
+        test(`Search keyword: ${testCase}`,
+            async ({homePage, searchPage}) => {
+                await homePage.searchField.fill(testCase)
+                await homePage.searchField.press("Enter")
+                expect(await (await searchPage.searchKeyword(testCase)).textContent()).toBe(`Search \"${testCase}\"`)
 
-            const result = await searchPage.getResult()
-            const invalidTitles = result.filter(
-                title => !title.toLowerCase().includes(testCase.toLowerCase())
-            )
+                const result = await searchPage.getResult()
+                const invalidTitles = result.filter(
+                    title => !title.toLowerCase().includes(testCase.toLowerCase())
+                )
 
-            expect(
-                invalidTitles,
-                `Some of titles don't have expected keyword:` +
-                `Expected: ${JSON.stringify(result, null, 2)}\n` +
-                `Actual: ${JSON.stringify(invalidTitles, null, 2)}`
-            ).toEqual([])
-        })
-
-    test("Navigate Top 250 Movies",
-        async ({homePage, searchPage}) => {
-            await homePage.menuButton.click()
-            await homePage.top250Button.click()
-            const firstMovieLocator = searchPage.getTheFirstMovie
-            await expect(await searchPage.getMovieTitle(firstMovieLocator)).toBeVisible()
-            await expect(await searchPage.getMovieRatingLocator(firstMovieLocator)).toBeVisible()
-            await expect(await searchPage.getMovieYearLocator(firstMovieLocator)).toBeVisible()
-        }
-    )
+                expect(
+                    invalidTitles,
+                    `Some of titles don't have expected keyword:` +
+                    `Expected: ${JSON.stringify(result, null, 2)}\n` +
+                    `Actual: ${JSON.stringify(invalidTitles, null, 2)}`
+                ).toEqual([])
+            })
+    })
 })
+
+test("Navigate Top 250 Movies",
+    async ({homePage, searchPage}) => {
+        await homePage.menuButton.click()
+        await homePage.top250Button.click()
+        const firstMovieLocator = searchPage.getTheFirstMovie
+        await expect(await searchPage.getMovieTitle(firstMovieLocator)).toBeVisible()
+        await expect(await searchPage.getMovieRatingLocator(firstMovieLocator)).toBeVisible()
+        await expect(await searchPage.getMovieYearLocator(firstMovieLocator)).toBeVisible()
+    }
+)
